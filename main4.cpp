e893b86268eea15d8383c7e01256e0ab5154c8f2
mrSvinka <181984796+mrSvinka@users.noreply.github.com>
2024-12-14 09:14:49 +0200

    ааааа

--- main4.cpp
+++ main4.cpp
@@ -1,47 +1,77 @@
+
+//4.   Сначала введите последовательность. Затем удалите и продублируйте элементы. 
+//     Затем выведите полученную последовательность (каждый элемент по одному разу).
+//     Используйте в программе только один массив.
+
+
 #include <iostream>
-#include <vector>
-#include <algorithm>
 using namespace std;
 
+const int MAX_SIZE = 100; 
+int main() 
+{
+    int n, a[MAX_SIZE];
 
+    // Ввод 
+    cout << "введите длину последовательности (n<=100): ";
+    cin >> n;
 
-int digitProduct(int num) {
-    int product = 1;
-    while (num > 0) {
-        product *= num % 10;
-        num /= 10;
-    }
-    return product;
-}
-
-int main() {
-    int n;
-    std::cout << "Введите количество чисел: ";
-    std::cin >> n;
-    std::vector<int> A(n);
-
-    std::cout << "Введите числа: ";
-    for (int i = 0; i < n; ++i) {
-        std::cin >> A[i];
+    if (n <= 0 || n > MAX_SIZE) 
+    {
+        cerr << "некорректная длина последовательности" << endl;
+        return 1;
     }
 
-    std::vector<int> result;
-    for (int num : A) {
-        int prod = digitProduct(num);
-        if (prod != 180) {
-            result.push_back(num);
-            if (std::to_string(num).front() == '1' && std::to_string(num).back() == '1') {
-                result.push_back(num);
+    cout << "введите последовательность: ";
+    for (int i = 0; i < n; i++) 
+    {
+        cin >> a[i];
+    }
+
+    // Удаление и дублирование 
+    for (int i = 0; i < n; i++) 
+    {
+        int temp = a[i];
+        for (int j = i; j < n - 1; j++) 
+        {
+            a[j] = a[j + 1];
+        }
+        a[n - 1] = temp;
+        i++;
+        for (int j = n - 1; j > i; j--) 
+        {
+            a[j] = a[j - 1];
+        }
+        a[i] = temp;
+        n++;
+    }
+
+    // Удаление 
+    for (int i = 0; i < n; i++) 
+    {
+        for (int j = i + 1; j < n;) 
+        {
+            if (a[j] == a[i]) 
+            {
+                for (int k = j; k < n - 1; k++) 
+                {
+                    a[k] = a[k + 1];
+                }
+                n--;
+            } else 
+            {
+                j++;
             }
         }
     }
 
-    std::sort(result.begin(), result.end());
-    result.erase(std::unique(result.begin(), result.end()), result.end());
-
-    std::cout << "Результат: ";
-    for (int num : result) {
-        std::cout << num << " ";
+    // Вывод 
+    cout << "последовательность: ";
+    for (int i = 0; i < n; i++) 
+    {
+        cout << a[i] << " ";
     }
+    cout << endl;
+
     return 0;
 }
