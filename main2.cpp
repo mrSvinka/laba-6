872f30bd8c704c55c0bafc47a5154088808290fa
mrSvinka <181984796+mrSvinka@users.noreply.github.com>
2024-12-14 08:35:31 +0200

    п

--- main2.cpp
+++ main2.cpp
@@ -1,50 +1,85 @@
+
 /*2. 	Ввести последовательность натуральных чисел {Aj}j=1...n (n<=1000). Упорядочить последовательность по неубыванию произведения цифр числа, 
 числа с одинаковыми произведениями цифр дополнительно упорядочить по неубыванию первой цифры числа, 
 числа с одинаковыми произведениями цифр и одинаковыми первыми цифрами дополнительно упорядочить по неубыванию самого числа.
 */
+
+
 #include <iostream>
-#include <vector>
 #include <algorithm>
 using namespace std;
 
-
-int digitProduct(int num) 
+// Произведение цифр числа
+int product_ch(int number) 
 {
     int product = 1;
-    while (num > 0) 
+    number = abs(number);
+    while (number > 0) 
     {
-        product *= num % 10;
-        num /= 10;
+        product *= number % 10;
+        number /= 10;
     }
     return product;
 }
 
+// Первая цифра числа
+int first(int number) 
+{
+    number = abs(number);
+    while (number >= 10) 
+    {
+        number /= 10;
+    }
+    return number;
+}
+
+// Компаратор для сортировки
+bool compare(int a, int b) 
+{
+    int prodA = product_ch(a);
+    int prodB = product_ch(b);
+    
+    if (prodA == prodB) 
+    {
+        int firstA = first(a);
+        int firstB = first(b);
+        
+        if (firstA == firstB) 
+        {
+            return a < b;  // Сравнение самих чисел
+        }
+        return firstA < firstB;  // Сравнение по первой цифре
+    }
+    return prodA < prodB;  // Сравнение по произведению
+}
+
+// Ввод
 int main() 
 {
+    const int MAX_N = 10000;
+    int numbers[MAX_N]; 
+
     int n;
     cout << "Введите количество чисел: ";
     cin >> n;
-    vector<int> A(n);
 
-    cout << "Введите числа: ";
     for (int i = 0; i < n; ++i) 
     {
-        cin >> A[i];
+        cout << "Введите число " << (i + 1) << ": ";
+        cin >> numbers[i];
     }
 
-    sort(A.begin(), A.end(), [](int a, int b) {
-        int prodA = digitProduct(a);
-        int prodB = digitProduct(b);
-        if (prodA != prodB) return prodA < prodB;
-        int firstA = std::to_string(a)[0] - '0';
-        int firstB = std::to_string(b)[0] - '0';
-        return (firstA != firstB) ? firstA < firstB : a < b;
-    });
+    // Сортировка
+    sort(numbers, numbers + n, compare);
 
-    cout << "Упорядоченная последовательность: ";
-    for (int num : A) 
+    // Вывод 
+    cout << "Последовательность: " << endl;
+    for (int i = 0; i < n; i++) 
     {
-        cout << num << " ";
+        cout << numbers[i] << " ";
     }
+    cout << endl;
+
     return 0;
 }
+
