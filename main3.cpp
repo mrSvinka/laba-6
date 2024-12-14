fc9e6f2106b9824274048d8b5e283f95981ca6c0
mrSvinka <181984796+mrSvinka@users.noreply.github.com>
2024-12-14 08:57:16 +0200

    п

--- main3.cpp
+++ main3.cpp
@@ -1,59 +1,69 @@
-//3. 	Дана целочисленная матрица {Aij}i=1..n,j=1..m (n,m<=100). Найти строку, в которой меньше всего четных чисел, и заменить все      элементы этой строки их квадратами.
+//3. 	Дана целочисленная матрица {Aij}i=1..n,j=1..m (n,m<=100). Найти строку, в которой меньше всего четных чисел, и заменить все элементы этой строки их квадратами.
 
 #include <iostream>
-#include <vector>
-#include <algorithm>
 using namespace std;
 
-
-int main() {
+int main() 
+{
     int n, m;
-    cout << "Введите размер матрицы (n): ";
+    cout << "Введите количество строк: ";
     cin >> n;
-    cout << "Введите размер матрицы (m): ";
-    cin >>m;
-    vector<vector<int>> A(n, vector<int>(m));
+    cout << "Введите количество столбцов: ";
+    cin >> m;
 
-    cout << "Введите элементы матрицы: ";
-    for (int i = 0; i < n; ++i) 
+
+    int A[n][m]; 
+
+    // Ввод 
+    cout << "Введите элементы матрицы: " << endl;
+    for (int i = 0; i < n; i++) 
     {
-        for (int j = 0; j < m; ++j) 
+        for (int j = 0; j < m; j++) 
         {
             cin >> A[i][j];
         }
     }
 
-    int minEvenCount = m + 1; // больше чем максимум
-    int minRowIndex = -1;
+    int min_count = m + 1; // Максимально возможное количество четных чисел
+    int min_row = -1; // Индекс строки с минимальным количеством четных
 
-    for (int i = 0; i < n; ++i) 
+    // Поиск строки с наименьшим количеством четных чисел
+    for (int i = 0; i < n; i++) 
     {
         int evenCount = 0;
-        for (int j = 0; j < m; ++j) 
+        for (int j = 0; j < m; j++) 
         {
-            if (A[i][j] % 2 == 0) evenCount++;
+            if (A[i][j] % 2 == 0) 
+            {
+                evenCount++;
+            }
         }
-        if (evenCount < minEvenCount) 
+        if (evenCount < min_count) 
         {
-            minEvenCount = evenCount;
-            minRowIndex = i;
+            min_count = evenCount;
+            min_row = i;
         }
     }
 
-    for (int j = 0; j < m; ++j) 
+    // Заменяем элементы найденной строки на их квадраты
+    if (min_row != -1) 
     {
-        A[minRowIndex][j] *= A[minRowIndex][j];
+        for (int j = 0; j < m; j++) 
+        {
+            A[min_row][j] = A[min_row][j] * A[min_row][j];
+        }
     }
 
-    cout << "Матрица после изменения: \n";
-    for (const auto& row : A) 
+    // Вывод 
+    cout << "Измененная матрица: " << endl;
+    for (int i = 0; i < n; i++) 
     {
-        for (int val : row) 
+        for (int j = 0; j < m; j++) 
         {
-            cout << val << " ";
+            cout << A[i][j] << " ";
         }
         cout << endl;
     }
+
     return 0;
 }
-
